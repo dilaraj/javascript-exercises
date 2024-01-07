652c5d10c1b2de66cd7ae96c008201fb80625690
Dilara Ajaj <da065@uowmail.edu.au>
2024-01-08 00:26:45 +1100

    Completing task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    const split = text.split("")
+    const newArray = split.reverse();
+    const newText = newArray.join("");
 
+    return newText
 };
 
 // Do not edit below this line
