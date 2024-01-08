d94835702368113cee8c6d633f7e64c70c12b182
Dilara Ajaj <da065@uowmail.edu.au>
2024-01-08 23:29:55 +1100

    Completing task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    const inBetween = [];
+    let total = 0;
+
+    if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
+        return 'ERROR';
+    } 
+
+    if (min < max) {
+        for (var i = min; i <= max; i++) {
+            inBetween.push(i);
+        }
+    } 
+    else if (min > max) {
+        for (var i = max; i <= min; i++) {
+            inBetween.push(i);
+        }
+    }
+
+    for (var i = 0; i < inBetween.length; i++) {
+        total += inBetween[i];
+    }
+    return total;
 
 };
 
