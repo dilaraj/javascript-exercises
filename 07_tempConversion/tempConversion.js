645eff65b7967c7aaf9d5101042ebe91c66d4d83
Dilara Ajaj <da065@uowmail.edu.au>
2024-03-11 02:00:33 +1100

    Completing temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const place = 1;
+const factor = Math.pow(10, place);
+
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * 5/9)*factor)/factor;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 9/5) + 32)*factor)/factor;
 };
 
 // Do not edit below this line
