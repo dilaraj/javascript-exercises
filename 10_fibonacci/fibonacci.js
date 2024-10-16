f646ab577133022de7b52b617baa93ac53b212fe
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2024-10-16 22:54:25 +0000

    Finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibonacci = [1, 1];
+    let i = fibonacci.length; // Length of array
+
+    if (num == 0) {
+        return 0;
+    } 
+    else if (num < 0) {
+        return 'OOPS';
+    } 
+
+    for (let j = i; j < num; j++) {
+        let latest = fibonacci[j-2] + fibonacci[j-1];
+        fibonacci.push(latest);
+    }
+
+    return fibonacci[num-1];
+
 
 };
+
+console.log(fibonacci(0));
 
 // Do not edit below this line
 module.exports = fibonacci;
