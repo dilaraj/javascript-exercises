00199fb4fedfc6dbcdf1d6d6ae7ecdb5f48c8987
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2024-10-16 23:24:20 +0000

    half of solution for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+const findTheOldest = function(obj) {
+
+    //return obj.map((person) => person.name);
+    // const oldest = obj.sort((a, b) => a.year > b.year ? 1 : -1);
+    // return oldest;
+    return obj
+        .sort((a, b) => a.yearOfBirth < b.yearOfBirth ? 1 : -1)
+        .map((person) => person.name);
 };
+
+//console.table(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
