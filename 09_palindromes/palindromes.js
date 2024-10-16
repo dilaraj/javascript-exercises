01cd3672d000cb784f4562a195119371414db9c0
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2024-10-16 08:44:03 +0000

    Finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (obj) {
+    // Remove punctuation and turn lowercase
+    let stripped = obj.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    //Remove spaces
+    let spaced = stripped.replace(/\s/g, '');
+    // Reverse the string
+    let reversed = spaced.split("").reverse().join("");
+
+    // boolean
+    if(reversed === spaced) { 
+        return true;
+    } 
+    else {
+        return false;
+    }
 
 };
+
+console.log(palindromes('A car, a man, a maraca.'))
 
 // Do not edit below this line
 module.exports = palindromes;
